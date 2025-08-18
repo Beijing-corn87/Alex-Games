185a53bdd8c30ab93b859b783fa9a8e5cc73dd5a
default <beijingcorn87@gmail.com>
2025-08-18 09:31:07 +0000

    added more spacing and took away some

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func displayGrid(numbers [9]string) {
 	for i := 0; i < len(numbers); i++ {
-		if (i+1)%3 == 0 {
+		if (i + 1)%3 == 0 {
 			fmt.Println(numbers[i])
 		} else {
 			if i != 0 {
@@ -21,8 +21,6 @@ func displayGrid(numbers [9]string) {
 				fmt.Print(numbers[i])
 				fmt.Print(" ")
 			}
-			
-			
 		}
 	}
 }
@@ -73,17 +71,24 @@ func main() {
 		// Setup
 		numbers := [9]string{"1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"}
 		displayGrid(numbers)
-		if currentPlayer == "❌" {
-			var usrPlace int
-			fmt.Print(currentPlayer, "'s turn: ")
-			_, err := fmt.Scan(&usrPlace)
-			if err != nil {
-				fmt.Println("Error reading input:", err)
-			} else {
-				numbers[usrPlace - 1] = currentPlayer
-				displayGrid(numbers)
-			}
+		var turnNum int
+		for turnNum < 9 {
+			if currentPlayer == "❌" {
+				var usrPlace int
+				fmt.Print(currentPlayer, "'s turn: ")
+				_, err := fmt.Scan(&usrPlace)
+				if err != nil {
+					fmt.Println("Error reading input:", err)
+				} else {
+					numbers[usrPlace - 1] = currentPlayer
+					clearConsole()
+					displayGrid(numbers)
+					turnNum++
+				}
+				
+			}			
 		}
+		
 
 	} else if mainChoice == 3 {
 		clearConsole()
